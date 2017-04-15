1340ddc740d2febe6a4370a0d8c5b96b3ac6e7d7
Neeky <1721900707@qq.com>
2017-04-15 20:08:49 +0800

    add user

--- main.sql
+++ main.sql
@@ -1,6 +1,18 @@
 -- 阿定的blog数据库模型
 
--- 建数据库dblog
+-- 0001建数据库dblog
 create database dblog char set utf8;
 
--- 
+-- 0002创建用户
+
+create user appuser@'localhost' identified by 'app_PASSWORD';
+create user appuser@'127.0.0.1' identified by 'app_PASSWORD';
+create user appuser@'%' identified by 'app_PASSWORD';
+
+-- 0003授权
+
+grant all on dblog.* to appuser@'localhost';
+grant all on dblog.* to appuser@'127.0.0.1';
+grant all on dblog.* to appuser@'%';
+
+-- 0004
